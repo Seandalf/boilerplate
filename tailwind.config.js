0173e391923b2fcde4c7a2cd5f2aa24547f11f69
Sean <sean@Seans-MacBook-Pro.local>
2021-12-25 22:52:10 +1100

    Fucked the mac

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './resources/js/**/**/*.vue',
+        './resources/js/**/**/**/*.vue',
     ],
 
     theme: {
